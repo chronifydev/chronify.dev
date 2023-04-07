bd454b973d75d15690a59f2612354aa55879e26e
Ignacio Van Droogenbroeck <ignacio@vandroogenbroeck.net>
2023-04-07 14:44:45 +0200

    built

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,4 +1,5 @@
 export default [
+  require('/home/nacho/wip/chronify/chronify.dev/node_modules/@docusaurus/plugin-google-gtag/lib/gtag'),
   require('/home/nacho/wip/chronify/chronify.dev/node_modules/infima/dist/css/default/default.css'),
   require('/home/nacho/wip/chronify/chronify.dev/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
   require('/home/nacho/wip/chronify/chronify.dev/node_modules/@docusaurus/theme-classic/lib/nprogress'),
--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,41 +3,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '578'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', 'd07'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', '67c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', '93a'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '82b'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', '0d1'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', '6f4'),
-    exact: true
-  },
-  {
     path: '/blog',
     component: ComponentCreator('/blog', '144'),
     exact: true
